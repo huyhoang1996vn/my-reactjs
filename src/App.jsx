31ce2d9755c515dcef5ad629a2754aebe095b950
huyhoang1996vn <huyhoang1996ha@gmail.com>
2022-04-25 09:53:19 +0700

    Apply amplify

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import CounterForm from './components/CounterRedux'
 import ContactForm from './components/Contact'
 import ContactRedux from './components/ContactRedux'
 import CounterReduxHook from './components/ContactReduxHook'
+import {Routes,Route,} from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
 
 
+Amplify.configure(awsExports);
+// https://docs.amplify.aws/start/getting-started/setup/q/integration/react/#install-amplify-libraries
 
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
-
-function App() {
+function App({ signOut, user }) {
   return (
     <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
       <Routes>
         <Route exact path="/game" element={<Game />} />
         <Route exact path="/" element={<Home />}/>
@@ -23,10 +27,11 @@ function App() {
         <Route exact path="/contact" element={<ContactForm />}/>
         <Route exact path="/contact-redux" element={<ContactRedux />}/>
         <Route exact path="/contact-redux-hook" element={<CounterReduxHook />}/>
-
       </Routes>
     </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
+// export default App;
